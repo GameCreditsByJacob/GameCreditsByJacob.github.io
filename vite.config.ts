9d96a5dd31f055c1228af021862710ad6bd9c2e9
Ralph Angelo Tan <ralphangelotan1011@gmail.com>
2024-05-25 03:29:55 +0800

    Moving the files for configuration in this branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,30 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+
 
 export default defineConfig({
   root: '.',
-  plugins: [react()],
+  base: '/GameCreditsByJacob.github.io',
+  plugins: [react()]
+  
 
-  server: {
-    host: 'localhost',
-    port: 5173,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:44308', // Replace with your backend URL
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: '/src/main.tsx',
-    },
-  },
+  // server: {
+  //   host: 'localhost',
+  //   port: 5173,
+  //   open: true,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:44308', // Replace with your backend URL
+  //       changeOrigin: true,
+  //       secure: false,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     },
+  //   },
+  // },
+  // build: {
+  //   outDir: 'dist',
+  //   rollupOptions: {
+  //     input: '/src/main.tsx',
+  //   },
+  // },
 });
