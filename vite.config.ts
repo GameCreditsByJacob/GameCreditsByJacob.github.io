db7fe82e140fcf2dd299a1d54c4c8b92dfcb8f8b
Ralph Angelo Tan <ralphangelotan1011@gmail.com>
2024-05-25 04:34:09 +0800

    final edit for the night

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from "vite";
 
 
-export default defineConfig({
+
+export default ({
   root: '.',
   base: '/GameCreditsByJacob.github.io',
   plugins: [react()]
