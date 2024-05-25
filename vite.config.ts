f99ddca5db4aa8a1d3cf684105730f80ca00cef1
Ralph Angelo Tan <ralphangelotan1011@gmail.com>
2024-05-26 01:31:01 +0800

    fixed defineConfig bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 
-export default({
+
+
+export default defineConfig({
   root: '.',
   base: '/GameCreditsByJacob.github.io/',
   plugins: [react()]
